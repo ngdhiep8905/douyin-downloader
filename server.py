a149542975f32d9001e385524a5d1e65bd1e5ed5
ngdhiep <hiepga24681@gmail.com>
2026-08-31 12:45:59 +0700

    Bypass YouTube Shorts bot sign-in check using tv_embedded player client

--- server.py
+++ server.py
@@ -88,7 +88,7 @@ def parse_ytdlp_media(url):
             'noplaylist': True,
             'extractor_args': {
                 'youtube': {
-                    'player_client': ['android'],
+                    'player_client': ['tv_embedded', 'android_vr', 'android'],
                 }
             }
         }
